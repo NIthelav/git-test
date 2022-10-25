0aee3fd992217f646cf94ee0ca10ef9cc7cc5062
Антипенко Валентин <nithelav@yandex.ru>
2022-10-25 22:59:30 +0300

    #2

---
+++ scripts/trackerMessage.js
@@ -0,0 +1,42 @@
+const axios = require("axios");
+
+if (!process.env.TOKEN) throw new Error("Нужно указать clientId");
+
+const OAuth = process.env.TOKEN;
+const tagName = process.env.TAGNAME;
+
+console.log(`OAuth ${OAuth}`);
+
+// const formater = new Intl.DateTimeFormat("en-US");
+
+// axios
+//   .patch(
+//     "https://api.tracker.yandex.net/v2/issues/HOMEWORKSHRI-158",
+//     {
+//       summary: `Релиз ${tagName} - ${formater.format(new Date())}`,
+//       description: "Новое описание задачи",
+//     },
+//     {
+//       headers: {
+//         Authorization: `OAuth ${OAuth}`,
+//         "X-Org-ID": 7526988,
+//       },
+//     }
+//   );
+
+// PATCH /v2/issues/<issue-id>
+// Host: https://api.tracker.yandex.net
+// Authorization: OAuth <OAuth-токен>
+// X-Org-ID: <идентификатор организации>
+// {
+//    Тело запроса в формате JSON
+// }
+
+// axios
+//   .get("https://api.tracker.yandex.net/v2/myself", {
+//     headers: {
+//       Authorization: `OAuth ${OAuth}`,
+//       "X-Org-ID": 7526988,
+//     },
+//   })
+//   .then(console.log);
